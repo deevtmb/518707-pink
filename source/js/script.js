f8d3e17128e35fc1cb9c17fd321db4683cf53cd1
Andrei <l2.ls@yandex.ru>
2018-11-05 19:18:39 +0300

    Правки по Б11 - лого не пропадает, десктоп на 960, изменен размер иконки-бургера

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@ var menu = document.querySelector(".main-nav");
 var header = document.querySelector(".page-header");
 var menuBg = document.querySelector(".page-header__wrapper");
 var menuToggle = document.querySelector(".page-header__toggle");
+var menuToggleImage = document.querySelector(".page-header__toggle-image");
 var cross = document.querySelector(".menu-cross");
 var burger = document.querySelector(".menu-burger");
 var popupSend = document.querySelector(".popup--send");
@@ -18,6 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
   header.classList.add("page-header--close");
   menuBg.classList.add("page-header__wrapper--bg");
   menuToggle.classList.add("page-header__toggle--js");
+  menuToggleImage.classList.add("page-header__toggle-image--burger");
   cross.style.display = "none";
 });
 
@@ -26,6 +28,7 @@ menuToggle.addEventListener("click", function(evt) {
   menu.classList.toggle("main-nav--close");
   header.classList.toggle("page-header--close");
   menuBg.classList.toggle("page-header__wrapper--bg");
+  menuToggleImage.classList.toggle("page-header__toggle-image--burger");
   if (menu.classList.contains("main-nav--close")) {
     cross.style.display = "none";
     burger.style.display = "block";
